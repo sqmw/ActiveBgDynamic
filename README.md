636acf0595178505dc0f43bb90502b2ce63399e1
sqmw <1951918362@qq.com>
2023-06-09 00:33:49 +0800

    old: Fixed an issue where the client's memory increased indefinitely over time

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # activeBgDynamic
 this project is written by tauri
 
-### what is tauri: 
-tauri is a fronted framework
+## what is tauri: 
+tauri is a fronted framework composed of webView(fronted) and rust(backend)
 
-### application
+## application
 This project is used to display a dynamic wallpaper which can interact with activeBg
+1. 这进程为activeBg的子进程，每隔周期性每隔100毫秒发起一次请求，获取activeBg需要本进程做的task，处理完之后返回结果
+2. 这个进程可以通过canvas提取视频的一帧，该视频仅仅是传输过来的链接，提取好一帧之后将这一帧图片编码成base64形式，返回给activeBg
+
+## 目前的功能，支持动态壁纸的展示，但是，可以添加subJoin的js代码，
+1. subJoin：表示的是附加的style，对应的是video(也就是动态视频壁纸)或者是和video同级的动态的图片动效壁纸
+
+## 完成部分的功能
--- src/components/ActiveTauriBg.vue
+++ src/components/ActiveTauriBg.vue
@@ -3,147 +3,156 @@
 </template>
 
 <script>
-  /**
-   *  backend response
-   *  regular activeDynamicBg communication format as follows
-   *  {
-   *    action: changeBg | executeScript | getImgFromVideo | mouseAction  ----> {changeBg: 0,executeScript:1, getImgFromVideo: 2}
-   *    data of each action
-   *      changeBg: url,
-   *      executeScript: script,
-   *      getImgFromVideo: ["url1","url2",...]
-   *      mouseAction: {
-   *        type: click | over | move,
-   *        // 对 domId 进行了处理，如果没有找到对应的id的 dom 就直接对首个canvas进行操作
-   *        domId: xxx,
-   *        point: {
-   *          x: 0,
-   *          y: 0
-   *        }
-   *      }
-   *
-   *
-   *  }
-   */
-  /**
-   * fronted reqData
-   * changeBg: none
-   * executeScript: none
-   * getImgFromVideo: return the img char stream encoded base64
-   * {
-   *   type: -1 -> rest  0 -> imgBase64
-   *   data: ""
-   * }
-   *
-   * data: imgBase64 -> data = {index:?, base64}
-   */
-  import { fetch, Body } from '@tauri-apps/api/http';
-  import {ref} from "vue";
-  export default {
-    name: "ActiveTauriBg",
-    setup(){
-      // scriptList, 用来存储保存的script id，new Date().getTime()
-      /**
-       * {
-       *   index:url,
-       * }
-       */
-      let videoUrlMap = {}
-      // 为了有更好的效果，图片应该一张一张地传输
-      // let videoResList = []
-      const req_typeRest = -1, req_typeImgBase64 = 0
-      const action_rest = -1, action_changeBg = 0,action_executeScript = 1, action_getImgFromVideo = 2, action_mouse = 3
-      const type_mouseActionClick = 1, type_mouseActionOver = 2, type_mouseActionMove = 4
-      /// 设置样式
-      const canvas = document.createElement("canvas");
-      const ctx = canvas.getContext("2d");
-      const video = document.createElement("video");
-      video.setAttribute('crossorigin', 'anonymous');
-      video.setAttribute('controls', 'controls');
-      video.currentTime = 1
-      let nowUrl = ref("https://img-baofun.zhhainiao.com/pcwallpaper_ugc/scene/b70351c2fe86b597c9265cfe7259ea4e_preview.mp4");
-      return {
-        nowUrl,
-        action_rest,
-        action_changeBg,
-        action_getImgFromVideo,
-        action_executeScript,
-        action_mouse,
-        video,
-        canvas,
-        videoUrlMap,
-        req_typeRest,
-        req_typeImgBase64,
-        ctx,
-        type_mouseActionClick,
-        type_mouseActionOver,
-        type_mouseActionMove
-      }
-    },
+/**
+ *  backend response
+ *  regular activeDynamicBg communication format as follows
+ *  {
+ *    action: changeBg | executeScript | getImgFromVideo | mouseAction  ----> {changeBg: 0,executeScript:1, getImgFromVideo: 2}
+ *    data of each action
+ *      changeBg: url,
+ *      executeScript: script,
+ *      getImgFromVideo: ["url1","url2",...]
+ *      mouseAction: {
+ *        type: click | over | move,
+ *        // 对 domId 进行了处理，如果没有找到对应的id的 dom 就直接对首个canvas进行操作
+ *        domId: xxx,
+ *        point: {
+ *          x: 0,
+ *          y: 0
+ *        }
+ *      }
+ *
+ *
+ *  }
+ */
+/**
+ * fronted reqData
+ * changeBg: none
+ * executeScript: none
+ * getImgFromVideo: return the img char stream encoded base64
+ * {
+ *   type: -1 -> rest  0 -> imgBase64
+ *   data: ""
+ * }
+ *
+ * data: imgBase64 -> data = {index:?, base64}
+ */
+import { fetch, Body } from '@tauri-apps/api/http';
+import {ref} from "vue";
+export default {
+  name: "ActiveTauriBg",
+  setup(){
+    // scriptList, 用来存储保存的script id，new Date().getTime()
+    /**
+     * {
+     *   index:url,
+     * }
+     */
+    let videoUrlMap = {}
+    // 为了有更好的效果，图片应该一张一张地传输
+    // let videoResList = []
+    const req_typeRest = -1, req_typeImgBase64 = 0
+    const action_rest = -1, action_changeBg = 0,action_executeScript = 1, action_getImgFromVideo = 2, action_mouse = 3
+    const type_mouseActionClick = 1, type_mouseActionOver = 2, type_mouseActionMove = 4
+    /// 设置样式
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    const video = document.createElement("video");
+    video.setAttribute('crossorigin', 'anonymous');
+    video.setAttribute('controls', 'controls');
+    video.currentTime = 1
+    let nowUrl = ref("https://img-baofun.zhhainiao.com/pcwallpaper_ugc/scene/b70351c2fe86b597c9265cfe7259ea4e_preview.mp4");
+    let fetchTimer
+    let fetchIntervalIsRun = false
+    return {
+      nowUrl,
+      action_rest,
+      action_changeBg,
+      action_getImgFromVideo,
+      action_executeScript,
+      action_mouse,
+      video,
+      canvas,
+      videoUrlMap,
+      req_typeRest,
+      req_typeImgBase64,
+      ctx,
+      type_mouseActionClick,
+      type_mouseActionOver,
+      type_mouseActionMove,
+      fetchTimer,
+      fetchIntervalIsRun
+    }
+  },
 
-    methods:{
-      change(receive){
-        this.nowUrl = receive
-      },
-      getBase64ImgCodeFromVideo(url,width=400,height=240) {
-        /// 返回一个 Promise
-        return new Promise((resolve, reject)=> {
-          /// 清除原来的画像
-          this.ctx.clearRect(0,0, width, height)
-          let dataURL = '';
-          this.video.setAttribute('src', url);
-          /// 这个是必须的，否则就是白屏
-          this.video.currentTime = 1
-          this.video.addEventListener('loadeddata', (e)=>{
-            this.canvas.width = width;
-            this.canvas.height = height;
-            this.canvas.getContext("2d").drawImage(this.video, 0, 0, width, height);
-            dataURL = this.canvas.toDataURL('image/png');
-            /// 返回的直接是 base64 编码
-            // console.log("base64编码",dataURL)
-            resolve(dataURL.split(",")[1]);
-          });
-        })
-      },
-      // type = 0(imgBase64), data: imgBase64 -> data = {index:?, base64}
-      // 这个连接仅仅是为了post msg
-      async postImgBase64(data = {}) {
-          /// 必须设置同步，一个一个处理
-        await fetch("http://localhost:4444/", {
-          timeout: 30,
-          method: "POST",
-          body: Body.json(data)
-        })
-      },
-      triggerMouseClickEvent(_dom,x = 0, y = 0){
-        _dom.dispatchEvent(new MouseEvent("click",{
-          view: window,
-          screenX: x,
-          screenY: y,
-        }))
-      },
-      triggerMouseOverEvent(_dom, x = 0, y = 0){
-        _dom.dispatchEvent(new MouseEvent("mouseover",{
-          screenX: x,
-          screenY: y,
-        }))
-      },
-      triggerMouseMoveEvent(_dom,x = 0, y = 0){
-        _dom.dispatchEvent(new MouseEvent("mousemove",{
-          screenX: x,
-          screenY: y,
-        }))
-      },
+  methods:{
+    change(receive){
+      this.nowUrl = receive
     },
-    async mounted() {
-      // 只有用来承接返回的数据
-      let resObj = {};
-      /// 返回给 backend 的data
-      let reqData = {
-        type: this.req_typeRest,
-        data: ""
+    getBase64ImgCodeFromVideo(url,width=400,height=240) {
+      /// 返回一个 Promise
+      return new Promise((resolve, reject)=> {
+        /// 清除原来的画像
+        this.ctx.clearRect(0,0, width, height)
+        let dataURL = '';
+        this.video.setAttribute('src', url);
+        /// 这个是必须的，否则就是白屏
+        this.video.currentTime = 1
+        this.video.addEventListener('loadeddata', (e)=>{
+          this.canvas.width = width;
+          this.canvas.height = height;
+          this.canvas.getContext("2d").drawImage(this.video, 0, 0, width, height);
+          dataURL = this.canvas.toDataURL('image/png');
+          /// 返回的直接是 base64 编码
+          // console.log("base64编码",dataURL)
+          resolve(dataURL.split(",")[1]);
+        });
+      })
+    },
+    // type = 0(imgBase64), data: imgBase64 -> data = {index:?, base64}
+    // 这个连接仅仅是为了post msg
+    async postImgBase64(data = {}) {
+      /// 必须设置同步，一个一个处理
+      await fetch("http://localhost:4444/", {
+        timeout: 30,
+        method: "POST",
+        body: Body.json(data)
+      })
+    },
+    triggerMouseClickEvent(_dom,x = 0, y = 0){
+      _dom.dispatchEvent(new MouseEvent("click",{
+        view: window,
+        screenX: x,
+        screenY: y,
+      }))
+    },
+    triggerMouseOverEvent(_dom, x = 0, y = 0){
+      _dom.dispatchEvent(new MouseEvent("mouseover",{
+        screenX: x,
+        screenY: y,
+      }))
+    },
+    triggerMouseMoveEvent(_dom,x = 0, y = 0){
+      _dom.dispatchEvent(new MouseEvent("mousemove",{
+        screenX: x,
+        screenY: y,
+      }))
+    },
+  },
+  async mounted() {
+    // 只有用来承接返回的数据
+    let resObj = {};
+    /// 返回给 backend 的data
+    let reqData = {
+      type: this.req_typeRest,
+      data: ""
+    }
+    const startFetchInterval = ()=>{
+      if (this.fetchIntervalIsRun){
+        return
       }
-      setInterval(async () => {
+      this.fetchIntervalIsRun = true
+      this.fetchTimer = setInterval(async () => {
         let script
         let scriptId
         let _dom
@@ -157,7 +166,7 @@
         }).then(async (res) => {
           resObj = res.data;
           switch (resObj["action"]) {
-            /// 修改背景
+              /// 修改背景
             case this.action_changeBg: {
               if (this.nowUrl !== resObj["data"]) {
                 this.nowUrl = resObj["data"]
@@ -166,9 +175,9 @@
               }
               break
             }
-            /// 执行脚本
-            /// 重新定义这里的规范
-            /// backend 会告诉前台需要的script的name XXX
+              /// 执行脚本
+              /// 重新定义这里的规范
+              /// backend 会告诉前台需要的script的name XXX
             case this.action_executeScript: {
               /// scriptName
               script = document.createElement("script")
@@ -186,7 +195,7 @@
               reqData["data"] = ""
               break
             }
-            /// 获取base64图片
+              /// 获取base64图片
             case this.action_getImgFromVideo: {
               /** 每次传输来的是一页的图片的链接
                * {
@@ -213,7 +222,7 @@
               reqData["data"] = ""
               break
             }
-            /// 因为各种原因，鼠标事件未能实现，后期需要根据钩子来完善
+              /// 因为各种原因，鼠标事件未能实现，后期需要根据钩子来完善
             case this.action_mouse: {
               _dom = document.getElementById(resObj["data"]["domId"])??document.querySelector("canvas")
               _clientX = resObj["data"]["point"]["x"]
@@ -231,7 +240,7 @@
                   this.triggerMouseMoveEvent(_dom,_clientX,_clientY)
                   break;
                 }
-                /// 表示两种情况
+                  /// 表示两种情况
                 case this.type_mouseActionMove | this.type_mouseActionClick:{
                   this.triggerMouseMoveEvent(_dom,_clientX,_clientY)
                   this.triggerMouseClickEvent(_dom,_clientX,_clientY)
@@ -247,7 +256,7 @@
                   this.triggerMouseClickEvent(_dom,_clientX,_clientY)
                   break;
                 }
-                /// 三种
+                  /// 三种
                 case this.type_mouseActionMove | this.type_mouseActionOver | this.type_mouseActionClick:{
                   this.triggerMouseMoveEvent(_dom,_clientX,_clientY)
                   this.triggerMouseOverEvent(_dom,_clientX,_clientY)
@@ -258,7 +267,7 @@
               /// 这里需要处理鼠标的点击事件，通过win32捕获之后发送过来
               break;
             }
-            /// 表示的是 rest
+              /// 表示的是 rest
             case this.action_rest:{
               /// 重置 resData
               reqData["type"] = this.req_typeRest
@@ -266,14 +275,21 @@
               break
             }
           }
-
         }).catch((err)=>{
-          //console.log(err)
+          clearInterval(this.fetchTimer)
+          console.log(err)
+          this.fetchIntervalIsRun = false
         })
-      },100)
+      },250)
     }
+    startFetchInterval()
+    // 通过两个套娃的 setInterval 解决了内存一直增加的问题
+    setInterval(()=>{
+      startFetchInterval()
+    },1000)
   }
-  /// npm run tauri dev
+}
+/// npm run tauri dev
 </script>
 
 <style>
